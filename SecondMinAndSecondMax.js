009150fd75bf6badc6c5a62d9f87173c5ec9bccf
dev01997D <devnandan01997@gmail.com>
2020-11-10 17:03:13 +0530

    Find second min and second max from the elements given into an array using sort function

--- SecondMinAndSecondMax.js
+++ SecondMinAndSecondMax.js
@@ -1,45 +1,21 @@
 /* Function to print second largest and second smallest elements */
-function print2Smallest(array) {
-    console.log(array);
-    let arr_size = array.length;
-
-    let firstMax = array[0];
-    let secondMax = Number.MIN_VALUE;
-    for (let index = 0; index < arr_size; index++) {
-        if (array[index] > firstMax) {
-            secondMax = firstMax;
-            firstMax = array[index];
-        }
-        // If array[index] is in between first and second then update second 
-        else if (array[index] > secondMax && array[index] < firstMax)
-            secondMax = array[index];
+{
+    function print2Smallest(array) {
+        console.log(array);
+        let sortedArray=array.sort(function(a, b){
+            return a-b
+        });
+        console.log(sortedArray);
+        let totalSize=array.length;
+        console.log("2nd largest : " + sortedArray[totalSize-1-1]);
+        console.log("2nd smallest : " + sortedArray[1]);
     }
-    console.log("2nd largest : " + secondMax);
 
-    let first = Number.MAX_VALUE;
-    let second = Number.MAX_VALUE;
-    for (let index = 0; index < arr_size; index++) {
-        if (array[index] < first) {
-            second = first;
-            first = array[index];
-        }
-        // If array[index] is in between first and second then update second 
-        else if (array[index] < second && array[index] > first)
-            second = array[index];
-
+    /* Driver program to test above functions */
+    let array = new Array();
+    for (let index = 0; index < 10; index++) {
+        let num = Math.floor(Math.random() * 1000);
+        array.push(num);
     }
-    console.log("2nd smallest : " + second);
+    print2Smallest(array);
 }
-
-/* Driver program to test above functions */
-// let array = new Array();
-// for (let index = 0; index < 10; index++) {
-//     let num = Math.floor(Math.random() * 1000);
-//     array.push(num);
-// }
-var myArray = [], X = 10;
-while (myArray.length < X) {
-    let num = Math.floor(Math.random() * 1000);
-  myArray.push(num);
-}
-print2Smallest(myArray); 
